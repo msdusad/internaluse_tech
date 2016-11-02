837e039b2ce644540ccc603b63293a7cfa31a221
Mehender Singh <mehender.singh@techvedic.co.in>
2016-11-02 14:38:32 +0530

    first commit

--- test.php
+++ test.php
@@ -32,3 +32,45 @@ $unserialized_array = unserialize($serialized_array);
 
 var_dump($serialized_array); // gives back a string, perfectly for db saving!
 var_dump($unserialized_array); //
+
+
+
+
+
+int main()
+{
+ int n, c, k, space = 1;
+
+ cout<<"\n\nEnter number of rows: ";
+ cin>>n;
+ space = n - 1;
+
+  for (k = 1; k<=n; k++)
+  {
+    for (c = 1; c<=space; c++)
+      cout<<" ";
+
+    space--;
+
+    for (c = 1; c<= 2*k-1; c++)
+      cout<<"*";
+
+    cout<<"\n";
+  }
+ 
+  space = 1;
+ 
+  for (k = 1; k<= n - 1; k++)
+  {
+    for (c = 1; c<= space; c++)
+      cout<<" ";
+ 
+    space++;
+ 
+    for (c = 1 ; c<= 2*(n-k)-1; c++)
+      cout<<"*";
+ 
+    cout<<"\n";
+  }
+ 
+}
