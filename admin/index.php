c72991a6341d9236845dbae2009890f99e68667b
msdusad <msdusad@gmail.com>
2016-12-29 16:14:51 +0530

    first commit

--- admin/index.php
+++ admin/index.php
@@ -28,23 +28,17 @@
               <div class="x_content">
                 <h1 class="h4 padding-bottom-10">Asset Management</h1>
 				<div class="row text-center">
-				     <a class="admin col-xs-6 col-sm-4 col-md-2" href="<?php echo WEB_ROOT;?>product-catalogue/index.php">
-					     <div class="circleBig">
-						     <div class="circle">
-							     <i class="icon-box"></i>
-							 </div>
-						 </div>
-						 <h4>Product Catalogue</h4>
-					 </a>
-					 <a class="admin col-xs-6 col-sm-4 col-md-2" href="#">
+				   
+					<!--   <a class="admin col-xs-6 col-sm-4 col-md-2" href="#">
 					     <div class="circleBig">
 						     <div class="circle">
 							     <i class="icon-home"></i>
 							 </div>
 						 </div>
 						 <h4>Vendors</h4>
-					 </a>
-					 <a class="admin col-xs-6 col-sm-4 col-md-2" href="#">
+					 </a> 					
+
+					  <a class="admin col-xs-6 col-sm-4 col-md-2" href="#">
 					     <div class="circleBig">
 						     <div class="circle">
 							     <i class="icon-slideshare"></i>
@@ -52,6 +46,18 @@
 						 </div>
 						 <h4>Relationship Types</h4>
 					 </a>
+
+<a class="admin col-xs-6 col-sm-4 col-md-2" href="#">
+					     <div class="circleBig">
+						     <div class="circle">
+							     <i class="icon-target"></i>
+							 </div>
+						 </div>
+						 <h4>Discovery</h4>
+					 </a>	 -->
+
+					
+
 					 <a class="admin col-xs-6 col-sm-4 col-md-2" href="<?php echo WEB_ROOT;?>assets/addnew.php">
 					     <div class="circleBig">
 						     <div class="circle">
@@ -68,17 +74,8 @@
 						 </div>
 						 <h4>Locations</h4>
 					 </a>
-					 <a class="admin col-xs-6 col-sm-4 col-md-2" href="#">
-					     <div class="circleBig">
-						     <div class="circle">
-							     <i class="icon-target"></i>
-							 </div>
-						 </div>
-						 <h4>Discovery</h4>
-					 </a>					
-				</div>
-				<div class="row text-center">
-				     <a class="admin col-xs-6 col-sm-4 col-md-2" href="<?php echo WEB_ROOT;?>admin/addgroup.php">
+					 
+					 <a class="admin col-xs-6 col-sm-4 col-md-2" href="<?php echo WEB_ROOT;?>admin/addgroup.php">
 					     <div class="circleBig">
 						     <div class="circle">
 							     <i class="icon-users"></i>
@@ -86,7 +83,7 @@
 						 </div>
 						 <h4>Groups</h4>
 					 </a>
-					 <div class="row text-center">
+					 
 				     <a class="admin col-xs-6 col-sm-4 col-md-2" href="<?php echo WEB_ROOT;?>admin/deparment.php">
 					     <div class="circleBig">
 						     <div class="circle">
@@ -111,7 +108,30 @@
 						 </div>
 						 <h4>Requestor</h4>
 					 </a>
-              </div>
+
+					   <a class="admin col-xs-6 col-sm-4 col-md-2" href="<?php echo WEB_ROOT;?>product-catalogue/index.php">
+					     <div class="circleBig">
+						     <div class="circle">
+							     <i class="icon-box"></i>
+							 </div>
+						 </div>
+						 <h4>Product Catalogue</h4>
+					 </a>
+
+					    <a class="admin col-xs-6 col-sm-4 col-md-2" href="<?php echo WEB_ROOT;?>asset-form/index.php">
+					     <div class="circleBig">
+						     <div class="circle">
+							     <i class="icon-users"></i>
+							 </div>
+						 </div>
+						 <h4>Asset Forms</h4>
+					 </a>
+					 			
+					 				
+				</div>
+				<!--<div class="row text-center">
+				     
+              </div-->
             </div>
         </div>
       </div>
--- classes/Connection.php
+++ classes/Connection.php
@@ -1,11 +1,12 @@
 <?php
-
+$link = mysqli_connect("localhost","root","","adminpanel") or die("Error " . mysqli_error($link));
 class Connection{
 private $host="localhost";
 private $user="root";
 private $password="";
 private $db="adminpanel";
 public  function __construct(){
+	
 mysql_connect($this->host,$this->user,$this->password) or die('Server Connection Error') ;
 return $this->db($this->db);
 }
